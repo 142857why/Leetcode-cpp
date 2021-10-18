bd0840fff7056b0cf95d1964235db33fe3fb4bb9
Rui Zhou <zhourui_1997@126.com>
2021-10-19 00:27:50 +0200

    Init 232 用栈实现队列

---
+++ 0232-implement-queue-using-stacks/main.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+class MyQueue {
+public:
+    MyQueue() {
+
+    }
+
+    void push(int x) {
+
+    }
+
+    int pop() {
+
+    }
+
+    int peek() {
+
+    }
+
+    bool empty() {
+
+    }
+};
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue* obj = new MyQueue();
+ * obj->push(x);
+ * int param_2 = obj->pop();
+ * int param_3 = obj->peek();
+ * bool param_4 = obj->empty();
+ */
