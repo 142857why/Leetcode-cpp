7f3c9ef7078f833cb806aa28543f74ac56a3b496
Rui Zhou <zhourui_1997@126.com>
2021-10-19 00:50:56 +0200

    Finish 232 用栈现实队列

--- 0232-implement-queue-using-stacks/main.cpp
+++ 0232-implement-queue-using-stacks/main.cpp
@@ -1,29 +1,76 @@
 #include <iostream>
+#include <stack>
+
+using namespace std;
 class MyQueue {
+private:
+    stack<int> inStack, outStack;
+
+    void in2Out() {
+        while (!inStack.empty()) {
+            outStack.push(inStack.top());
+            inStack.pop();
+        }
+    }
 public:
     MyQueue() {
 
     }
 
     void push(int x) {
-
+        inStack.push(x);
     }
 
     int pop() {
-
+        if (outStack.empty()) {
+            in2Out();
+        }
+        int x = outStack.top();
+        outStack.pop();
+        return x;
     }
 
     int peek() {
-
+        if (outStack.empty()) {
+            in2Out();
+        }
+        return outStack.top();
     }
 
     bool empty() {
+        return (inStack.empty() && outStack.empty());
+    }
 
+    void print() {
+        stack<int> tempIn(inStack);
+        stack<int> tempOut(outStack);
+        cout << "----InStack----" << endl;
+        while (!tempIn.empty()) {
+            cout << tempIn.top() << " ";
+            tempIn.pop();
+        }
+        cout << "\n----------------" << endl;
+
+        cout << "----OutStack----" << endl;
+        while (!tempOut.empty()) {
+            cout << tempOut.top() << " ";
+            tempOut.pop();
+        }
+        cout << "\n-----------------" << endl;
     }
 };
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    MyQueue* obj = new MyQueue();
+    obj->push(1);
+    obj->push(2);
+    obj->push(3);
+    obj->push(4);
+    obj->print();
+    cout << obj->pop() << endl;
+    cout << obj->peek() << endl;
+    obj->push(5);
+    obj->print();
     return 0;
 }
 
