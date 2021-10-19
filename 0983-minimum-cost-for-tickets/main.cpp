aa5ca70fb69e080c1252534ad4712afd235d75de
Rui Zhou <zhourui_1997@126.com>
2021-10-19 17:41:26 +0200

    Init 983-最低票价

---
+++ 0983-minimum-cost-for-tickets/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+class Solution {
+public:
+    int mincostTickets(vector<int>& days, vector<int>& costs) {
+
+    }
+};
+
+int main() {
+    
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
