e5768ff78b3950afe22f32ba10677857d2eeae8d
Rui Zhou <zhourui_1997@126.com>
2021-10-22 19:17:29 +0200

    0937 初始化输入输出

--- 0937-reorder-data-in-log-files/main.cpp
+++ 0937-reorder-data-in-log-files/main.cpp
@@ -1,6 +1,19 @@
 #include <iostream>
+#include <string>
+#include <vector>
 
+using namespace std;
+class Solution {
+public:
+    vector<string> reorderLogFiles(vector<string>& logs) {
+
+    }
+};
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    vector<string> logs{"dig1 8 1 5 1","let1 art can","dig2 3 6","let2 own kit dig","let3 art zero"};
+    vector<string> ans = (new Solution())->reorderLogFiles(logs);
+    for (auto x : ans) {
+        cout << x << endl;
+    }
     return 0;
 }
