6330d8d7e7861e9aef8316500bb5df8089228c46
Rui Zhou <zhourui_1997@126.com>
2021-10-30 01:05:23 +0200

    Init 869-重排成2的幂次

---
+++ 0869-reordered-power-of-2/main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+    bool reorderedPowerOf2(int n) {
+
+    }
+};
+
+int main() {
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
