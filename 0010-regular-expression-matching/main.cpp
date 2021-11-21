2522dd41d02ee6a0167a9cf92dcf530ccca61138
Rui Zhou <zhourui_1997@126.com>
2021-11-21 16:53:25 +0100

    Finish 10-正则表达式匹配
    44题与这题类似

--- 0010-regular-expression-matching/main.cpp
+++ 0010-regular-expression-matching/main.cpp
@@ -4,16 +4,56 @@
 
 using namespace std;
 class Solution {
+private:
+    unordered_map<string, bool> memo;
+
+    bool dp(const string& s, int i, const string& p, int j) {
+        if (j == p.size()) return (i == s.size());
+        if (i == s.size()) {
+            if ((p.size() - j) % 2) return false; // the remaining part of the pattern string must have even length
+            while (j < p.size()) {
+                if (p[j + 1] != '*') return false;
+                j += 2;
+            }
+            return true;
+        }
+
+        string key = to_string(i) + ","  + to_string(j);
+        if (memo.count(key)) return memo[key];
+
+        bool res = false;
+        if (s[i] == p[j] || p[j] == '.') {
+            // match at the current position
+            if (j < p.size() - 1 && p[j+1] == '*') {
+                // now the next character is '*', we need to check whether it can match 0 or many times
+                res = dp(s, i, p, j+2) || dp(s, i+1, p, j);
+            } else {
+                // single match, move both i and j
+                res = dp(s, i+1, p, j+1);
+            }
+        } else {
+            // doesn't match at the current position
+            if (j < p.size() - 1 && p[j+1] == '*') {
+                // now the next character is '*', this saves s[i] and p[j] temporarily, we need to skip the '*'
+                res = dp(s, i, p, j + 2);
+            } else {
+                // no one can save the matching now
+                res = false;
+            }
+
+        }
+        return memo[key] = res;
+    }
 public:
     bool isMatch(string s, string p) {
-        return false;
+        return dp(s, 0, p, 0);
     }
 };
 
 int main() {
     auto* obj = new Solution();
-    string s = "";
-    string p = "";
+    string s = "aab";
+    string p = "c*a*b";
     bool f = obj->isMatch(s, p);
     if (f) {
         cout << "true" << endl;
