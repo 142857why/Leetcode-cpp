462b15fd56ed95920cf152c18b20f38a2d64b323
Rui Zhou <zhourui_1997@126.com>
2021-11-22 11:45:03 +0100

    Finish 286-墙与门
    多源同时BFS

--- 0286-walls-and-gates/main.cpp
+++ 0286-walls-and-gates/main.cpp
@@ -1,10 +1,48 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 using namespace std;
+
 class Solution {
+
+#define WALL (-1)
+#define DOOR (0)
+#define EMPTY_ROOM (2147483647)
+
+private:
+    // up, down, left, right
+    int direction[4][2] = {
+        {1, 0},
+        {-1, 0},
+        {0, -1},
+        {0, 1},
+    };
+    queue<pair<int, int>> q;
+
+
 public:
     void wallsAndGates(vector<vector<int>>& rooms) {
+        int m = rooms.size();
+        if (!m) return;
+        int n = rooms[0].size();
+        if (!n) return;
+        for (int i = 0; i < m; ++i) {
+            for (int j = 0; j < n; ++j) {
+                if (rooms[i][j] == DOOR) q.emplace(make_pair(i, j));
+            }
+        }
 
+        while (!q.empty()) {
+            auto [row, col] = q.front(); q.pop();
+            for (auto & dir : direction) {
+                int newRow = row + dir[0];
+                int newCol = col + dir[1];
+
+                if (newRow < 0 || newRow >= m || newCol < 0 || newCol >= n || rooms[newRow][newCol] != EMPTY_ROOM) continue;
+                rooms[newRow][newCol] = rooms[row][col] + 1;
+                q.emplace(make_pair(newRow, newCol));
+            }
+        }
     }
 };
 
